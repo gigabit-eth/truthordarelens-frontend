7b7f5ef4b6b0b3572838b86a5b8acf2b0e9766b0
gigabit-eth <ogpuzzle9@gmail.com>
2023-07-01 18:01:47 -0700

    initial commit

--- utils/config.ts
+++ utils/config.ts
@@ -1,48 +1,47 @@
 import {
-	mainnet,
-	polygon,
-	sepolia,
-	polygonMumbai,
-	polygonZkEvm,
-	polygonZkEvmTestnet,
-	bsc,
-	bscTestnet,
-	arbitrum,
-	arbitrumGoerli,
-} from 'wagmi/chains';
+  mainnet,
+  polygon,
+  sepolia,
+  polygonMumbai,
+  polygonZkEvm,
+  polygonZkEvmTestnet,
+  bsc,
+  bscTestnet,
+  arbitrum,
+  arbitrumGoerli,
+} from "wagmi/chains";
 
 export const ETH_CHAINS = [
-	mainnet,
-	polygon,
-	sepolia,
-	polygonMumbai,
-	polygonZkEvm,
-	polygonZkEvmTestnet,
-	bsc,
-	bscTestnet,
-	arbitrum,
-	arbitrumGoerli,
+  mainnet,
+  polygon,
+  sepolia,
+  polygonMumbai,
+  polygonZkEvm,
+  polygonZkEvmTestnet,
+  bsc,
+  bscTestnet,
+  arbitrum,
+  arbitrumGoerli,
 ];
 export const WALLET_CONNECT_PROJECT_ID =
-	process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '';
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "";
 
-export const SITE_NAME = 'boilr3';
+export const SITE_NAME = "Truthordare.lens";
 export const SITE_DESCRIPTION =
-	'The Ultimate Next.js dApp Boilerplate with RainbowKit, Tailwind CSS & WAGMI';
-export const SITE_URL = 'https://boilr3.vercel.app';
+  "Get to know your friends better, or make new ones, the game of truth or dare for the Lens Protocol. ";
+export const SITE_URL = "https://truthordare.lol";
 
-export const SOCIAL_TWITTER = 'Envoy_1084';
-export const SOCIAL_GITHUB = 'Envoy-VC/boilr3';
+export const SOCIAL_TWITTER = "Envoy_1084";
+export const SOCIAL_GITHUB = "Envoy-VC/boilr3";
 
 export const NFT_CONTRACT_ADDRESS =
-	'0x0Fc5f8A784810dEd101BD734cC59F6F7b868A3AF';
+  "0x0Fc5f8A784810dEd101BD734cC59F6F7b868A3AF";
 
 export const ironOptions = {
-	cookieName: SITE_NAME,
-	password:
-		process.env.SESSION_PASSWORD ??
-		'set_a_complex_password_at_least_32_characters_long',
-	cookieOptions: {
-		secure: process.env.NODE_ENV === 'production',
-	},
+  cookieName: SITE_NAME,
+  password:
+    process.env.SESSION_PASSWORD ?? "HB$TohaAGR5s!m7Q!x&7&?oMxyM#sgckihk&h3r@",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
 };
