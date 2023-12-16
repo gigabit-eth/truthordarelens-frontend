dcde0c5a64038d196ac7bd4cde32639d52d7bcf2
gigabit-eth <ogpuzzle9@gmail.com>
2023-12-16 07:27:55 -0800

    light mode terminal

--- components/ui/Terminal.tsx
+++ components/ui/Terminal.tsx
@@ -22,7 +22,7 @@ const TerminalContact = () => {
       onClick={() => {
         inputRef.current?.focus();
       }}
-      className="h-96 bg-slate-950/70 backdrop-blur rounded-lg w-full max-w-3xl mx-auto overflow-y-scroll shadow-xl cursor-text font-mono"
+      className="h-96 bg-[#DED6C8]/90 dark:bg-slate-950/70 backdrop-blur rounded-lg w-full max-w-3xl mx-auto overflow-y-scroll shadow-xl cursor-text font-mono"
     >
       <TerminalHeader />
       <TerminalBody inputRef={inputRef} containerRef={containerRef} />
@@ -32,12 +32,12 @@ const TerminalContact = () => {
 
 const TerminalHeader = () => {
   return (
-    <div className="w-full p-3 bg-slate-900 flex items-center gap-1 sticky top-0">
+    <div className="w-full p-3 bg-[#A79E97] dark:bg-slate-900 flex items-center gap-1 sticky top-0">
       <div className="w-3 h-3 rounded-full bg-red-500" />
       <div className="w-3 h-3 rounded-full bg-yellow-500" />
       <div className="w-3 h-3 rounded-full bg-green-500" />
-      <span className="text-sm text-slate-200 font-semibold absolute left-[50%] -translate-x-[50%]">
-        help@truthordare.lol
+      <span className="text-sm text-[#260F1F] dark:text-slate-200 font-semibold absolute left-[50%] -translate-x-[50%]">
+        👩‍💻 add new questions to the database queue
       </span>
     </div>
   );
@@ -94,8 +94,10 @@ const TerminalBody = ({ containerRef, inputRef }: TerminalBodyProps) => {
 const InitialText = () => {
   return (
     <>
-      <p>Howdy! You ready to create some questions? 🤠 </p>
-      <p className="whitespace-nowrap overflow-hidden font-light">
+      <p className="text-[#CA2014] dark:text-slate-200">
+        Hey! You ready to spark some conversations? 🤠{" "}
+      </p>
+      <p className="whitespace-nowrap overflow-hidden font-light text-[#999376] dark:text-slate-200">
         ------------------------------------------------------------------------
       </p>
     </>
@@ -173,7 +175,7 @@ const Summary = ({ questions, setQuestions }: SummaryProps) => {
       {complete ? (
         <p className="text-emerald-300">
           <FiCheckCircle className="inline-block mr-2" />
-          <span>Sent! We&aptos;ll get back to you ASAP 😎</span>
+          <span>One more step! We have to save it to the blockchain 😎</span>
         </p>
       ) : (
         <div className="flex gap-2 mt-2">
